8e1ab0c79bd59d06405741722a95da9294fcda68
kv83 <kv666@mail.ru>
2020-09-23 18:13:09 +0300

    Изменение файлов

--- 1.php
+++ 1.php
@@ -6,5 +6,8 @@
     <body>
     <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1>
     <h2><?php echo 'Информация о файле 1.html' ?></h2>
+    <a class="list" href="index.html">Перейти на главную страницу</a>
+    <a class="list" href="1.html">Перейти на страницу 1</a>
+    <a class="list" href="2.html">Перейти на страницу 2</a>
     </body>
     </html>
