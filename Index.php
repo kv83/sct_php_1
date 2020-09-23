3f699d448cffe1f6ac44408db535cfd4808d9815
kv83 <kv666@mail.ru>
2020-09-23 18:16:06 +0300

    Изменение файлов

--- Index.php
+++ Index.php
@@ -3,10 +3,10 @@
        <head>
            <title>Document</title>
        </head>
-             <body>
+          <body>
              <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1>
              <a class="list" href="index.html">Перейти на главную страницу</a>
              <a class="list" href="1.html">Перейти на страницу 1</a>
              <a class="list" href="2.html">Перейти на страницу 2</a>
-             </body>
+          </body>
     </html>
