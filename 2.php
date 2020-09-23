434f5259ffad92e5ab4c51b3fb635438dc7830e8
kv83 <kv666@mail.ru>
2020-09-23 18:11:32 +0300

    Изменение файлов

---
+++ 2.php
@@ -0,0 +1,10 @@
+<doctype html>
+    <html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+    <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1>
+    <h2><?php echo 'Информация о файле 2.html' ?></h2>
+    </body>
+    </html>
---
+++ Index.php
@@ -0,0 +1,12 @@
+<doctype html>
+    <html>
+       <head>
+           <title>Document</title>
+       </head>
+             <body>
+             <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1>
+             <a class="list" href="index.html">Перейти на главную страницу</a>
+             <a class="list" href="1.html">Перейти на страницу 1</a>
+             <a class="list" href="2.html">Перейти на страницу 2</a>
+             </body>
+    </html>
