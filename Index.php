ce3fa556b9578d4d4a61bd03307c5deb8edf3784
kv83 <kv666@mail.ru>
2020-09-23 19:25:02 +0300

    Конечный результат

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
           <body>
              <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1></br>
              <a class="list" href="index.php">Перейти на главную страницу</a></br>
-             <a class="list" href="1.php">Перейти на страницу 1</a></br>
-             <a class="list" href="2.php">Перейти на страницу 2</a>
+             <a class="list" href="php/1.php">Перейти на страницу 1</a></br>
+             <a class="list" href="php/2.php">Перейти на страницу 2</a>
           </body>
     </html>
