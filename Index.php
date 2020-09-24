3ebda5efbee661cae8eeec1ed40df774cb52cdea
kv83 <kv666@mail.ru>
2020-09-24 12:16:10 +0300

    Страница с картинкой

--- Index.php
+++ Index.php
@@ -1,12 +1,21 @@
 <doctype html>
     <html>
        <head>
-           <title>Document</title>
+           <title><?= 'Скрипт'?></title>
        </head>
           <body>
-             <h1><?php echo 'Категорический привет читателю данного текста!' ?></h1></br>
-             <a class="list" href="index.php">Перейти на главную страницу</a></br>
-             <a class="list" href="php/1.php">Перейти на страницу 1</a></br>
-             <a class="list" href="php/2.php">Перейти на страницу 2</a>
+             <?php
+              echo '<h1>Категорический привет читателю данного текста!</h1>';
+              echo '<p>Данный текст предназначен для ознакомления.</p>';
+              echo '<p>Ознакомились. Запомнили. Отлично!</p>';
+              echo '<p>Хорошего дня!</p>';
+              ?>
+             <img src="<? echo '123.jpg';?>" width="300" height="250" alt="Картинка"></br>
+             <a href="<?= 'index.php'?>">Перейти на главную страницу</a></br>
+             <a href="<?= 'php/1.php'?>">Перейти на страницу 1</a></br>
+             <a href="<?= 'php/2.php'?>">Перейти на страницу 2</a>
+               <script>
+                   var a = '<?php echo 12?>';
+               </script>
           </body>
     </html>
--- php/2.php
+++ php/2.php
@@ -8,8 +8,8 @@
             <p> Text </p>
              <h2><?php echo 'Информация о файле 2.php' ?></h2></br>
              <p> Text file 2</p>
-             <a class="list" href="../index.php">Перейти на главную страницу</a></br>
-             <a class="list" href="1.php">Перейти на страницу 1</a></br>
-             <a class="list" href="2.php">Перейти на страницу 2</a>
+            <a href="<?= '/index.php'?>">Перейти на главную страницу</a></br>
+            <a href="<?= '1.php'?>">Перейти на страницу 1</a></br>
+            <a href="<?= '2.php'?>">Перейти на страницу 2</a>
            </body>
     </html>
