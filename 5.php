f5857391545890b00355777b3dd19a3f1eab6a24
kv83 <kv666@mail.ru>
2020-11-21 20:20:47 +0300

    Переделка с 14 урока 3.php

---
+++ 5.php
@@ -0,0 +1,29 @@
+<?php
+    function page(){
+    $page = "<html>
+    <head>
+    <title>{{TITLE}}</title>
+    </head>
+    <body>
+    {{CONTENT}}
+    </body>
+    </html>";
+    return $page;
+    }
+        function replaceTitle($html, $newTitle){
+            return str_replace("{{TITLE}}", $newTitle, $html);
+        }
+            function replaceContent($html, $newContent){
+                return str_replace("{{CONTENT}}", $newContent, $html);
+    }
+                function getReplace($page, $element, $replacement){
+                    switch($element){
+                        case "title" : return replaceTitle($page, $replacement);break;
+                        case "content" : return replaceContent($page, $replacement);break;
+                        default : return "";break;
+    }
+}
+$page = page();
+$page = getReplace($page, "title", "this is new title");
+$page = getReplace($page, "content", "this is new content");
+echo $page;
