44852632371f3980f2eb3ea4cd34b91ca2b362de
kv83 <kv666@mail.ru>
2020-11-26 18:13:31 +0300

    Вроде всё.

--- 1.php
+++ 1.php
@@ -5,26 +5,28 @@
 </head>
 <body>
 <?php
-    function vozvrat ()
-    {
-        $a=12;
-        $b=12;
-        if ($a==$b) {
-            echo 'Числа равны ';
-        }
-        while ($a > $b) {
-            echo $b++. '<br>';
-            if ($a==$b) break;
-        }
-        while ($a < $b) {
-            echo $a++. '<br>';
-            if ($a==$b) break;
-            }
-        if ($a=$b) {
-            echo $b;
-        }
+function vozvrat ()
+{
+    $a=20;
+    $b=20;
+    while ($a==$b){
+        echo 'Числа равны';
+        exit;
     }
-    vozvrat ();
+    while ($a > $b) {
+        echo $b++. '<br>';
+        if ($a==$b)
+            echo $a;
+        if ($a<$b) exit;
+    }
+    while ($a < $b) {
+        echo $a++. '<br>';
+        if ($a==$b)
+            echo $b;
+        if ($a>$b) exit;
+    }
+}
+vozvrat ();
 ?>
 </body>
 </html>
