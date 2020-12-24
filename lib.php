ba6376e53288891eae6cf8676ee10ca2d4fff4d0
kv83 <kv666@mail.ru>
2020-12-24 15:09:09 +0300

    lesson_20

--- lib.php
+++ lib.php
@@ -15,13 +15,12 @@ function getPage ()
     } elseif (!is_numeric($_GET['page']) or $_GET["page"] ==""){
         include 'page/1.php';
     }
-    echo '<br>Передано методом '.$_SERVER['REQUEST_METHOD'];
 }
 getPage();
     function logic ()
     {
         $file = "log.txt";
-        $col_zap = 999;
+        $col_zap = 4999;
         $date = date("H:i:s d.m.Y");
         $home = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $lines = file($file);
